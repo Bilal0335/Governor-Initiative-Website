733f1698f7763babe426aedfd1cc69b94ab6e14c
Bilal0335 <bilalhussain42201.com>
2024-11-19 22:23:55 +0500

    😄update😄

--- src/app/component/Slider/index.tsx
+++ src/app/component/Slider/index.tsx
@@ -4,14 +4,24 @@ import { Component, ReactNode } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import Image from "next/image";
 import Image1 from "@/assets/img/image10.jpg"; 
+import Image2 from "@/assets/img/image20.jpg"; 
+import Image3 from "@/assets/img/image30.jpg";
 
+import Image from "next/image";
 export default class ImagesCarousel extends Component {
   render(): ReactNode {
     const data = [
       {
         img: Image1,
+        title: "Student learning Web3 basics"
+      },
+      {
+        img: Image2,
+        title: "Student learning Web3 basics"
+      },
+      {
+        img: Image3,
         title: "Student learning Web3 basics"
       },
       {
@@ -50,16 +60,17 @@ export default class ImagesCarousel extends Component {
         <Slider {...settings} className="w-full">
           {data.map((val, index) => (
             <div
+              className="relative rounded-lg overflow-hidden cursor-pointer outline-none"
               key={index}
-              className="relative bg-slate-900 rounded-lg overflow-hidden cursor-pointer outline-none"
             >
-              <div className="px-3 xl-lg:px-1 group mb-2 md:mb-5 relative"></div>
-              <div className="relative bg-slate-900 rounded-lg overflow-hidden">
-                <Image
-                  src={val.img}
-                  className="w-full h-[300px] md:h-[600px] object-cover"
-                  alt={val.title}
-                />
+              <div className="px-3 xl-lg:px-1 group mb-2 md:mb-5 relative">
+                <div className="relative rounded-lg overflow-hidden flex gap-3">
+                  <Image
+                    src={val.img}
+                    className="w-full h-[300px] md:h-[600px] object-cover"
+                    alt={val.title}
+                  />
+                </div>
               </div>
             </div>
           ))}
