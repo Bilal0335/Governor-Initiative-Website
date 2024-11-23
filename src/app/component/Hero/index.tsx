7f2d81ca44705225a0aafc2f73da4e89bb3c686f
Bilal0335 <bilalhussain42201.com>
2024-11-23 10:15:09 +0500

    😄update😄

--- src/app/component/Hero/index.tsx
+++ src/app/component/Hero/index.tsx
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 import React from "react";
 const popins = Poppins({
   weight: ["300", "400", "500", "800", "900"],
@@ -8,7 +9,7 @@ const popins = Poppins({
 const Hero = () => {
   return (
     <div
-      className={`relative z-0 w-full overflow-hidden bg-opacity-75 bg-[url('../governor-house.jpg')] bg-cover ${popins.className}`}
+      className={`relative z-0 w-full overflow-hidden bg-opacity-75 bg-[url('/assets/img/governor-house.jpg')] bg-cover ${popins.className}`}
     >
       <div className="absolute inset-0 h-full w-full bg-zinc-100 -z-50 bg-opacity-80"></div>
       <div className="m-auto z-50 mt-6 w-[95%] block justify-between items-center md:flex-row lg:w-[90%] xl:w-[1300px]">
@@ -24,11 +25,19 @@ const Hero = () => {
             Applied Generative AI <br />
             Engineer (GenEng)
           </h1>
-          <p className="text-main whitespace-nowrap text-center tex-[2.5rem] font-extrabold sm:2xl md:text-left">
+          <p className="my-5 text-main whitespace-nowrap text-center tex-[1.25rem] font-extrabold sm:text-2xl md:text-left">
             Earn up to $5,000 / month
-            <br />
+          </p>
+          <p className="my-5 text-main w-full text-center tex-[1.25rem] font-bold tracking-wider md:w-[80%] sm:text-2xl md:text-left">
             Now admissions are open in Hyderabad
           </p>
+          <div className="mb-7 mt-5 flex flex-col items-center sm:mt-10 sm:flex-col sm:gap-5 md:mb-0">
+            <Link href={"/apply"} className="w-full md:w-auto">
+              <button className=" w-full rounded-md:text-main text-center text-sm py-3 font-semibold tracking-widest text-white transition-all hover:translate-y-1 sm:py-4 sm:text-base md:w-52">
+                Apply Now
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
